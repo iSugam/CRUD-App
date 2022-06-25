fbca5b69fd9278fe06b0d535844cdb27cd8a0b7e
unknown <sugamanandsahu01@gmail.com>
2022-06-25 16:31:58 +0530

    Still working on editing and updating the article. Bug:- Making a complete new article on update.

--- routes/article.js
+++ routes/article.js
@@ -2,42 +2,61 @@ const express = require('express');
 const router = express.Router();
 const Article = require('.././mongoose');
 
+// Get the article posting Form route
 router.get("/", (req, res) => {
-    res.render("./postOrEdit/postOrEdit", {formHeading: "Post a new Article", article:""})
+    res.render("./postAndEdit/post");
 })
 
-
+// For Posting an Article
 router.post("/", (req, res) => {
+
     let data = new Article({
         title : req.body.title,
         publishDate: new Date().toLocaleDateString(),
         description: req.body.description
     })
-    if(req.body._id !== ""){
+    data.save(err => console.log(err));
+    res.redirect(".././");
+})
 
-        Article.findOneAndUpdate(req.body._id, req.body);
-        res.redirect(".././")
-       
+// Get the article Editing Form route
+router.get("/:id",  (req, res) => {
+    let articleID = req.params.id;
+
+  try { 
+     Article.findById(articleID, (err, foundArticle) => {
+        res.render("./postAndEdit/edit", {article: foundArticle});
+    })
+        
+}catch(err) {
+        console.log(err);
+    }
+})
+
+// For Updating an Article
+router.post("/:id", async (req, res) => {
+    let articleID = req.body._id;
+
+    try {
+        await Article.findByIdAndUpdate(articleID, {title: req.body.title, description: req.body.description}, {returnOriginal: true});
+        res.redirect("/");
+    }   
+    catch (error) {
+        console.log(error);
     }
 
-   else{
-    data.save(err => console.log(err));
-    res.redirect(".././")
-   }
-  
-     
 })
 
-router.get("/:id", (req, res) => {
-    let articleID = req.params.id;
-    console.log(articleID);
-
-    Article.findById({_id: articleID}, (err, foundID) => {
-        if(!err) res.render("./postOrEdit/postOrEdit", {formHeading: "Update the post", article: foundID})
-
-    })
+// Fro Deleting an Article
+router.get("/delete/:id", async (req, res) => {
+    try {
+        await Article.findOneAndRemove({_id: req.params.id});
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+    }
+   
 })
 
-
-
-module.exports = router
+// Export the module
+module.exports = router;
